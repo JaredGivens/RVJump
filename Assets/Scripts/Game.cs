c8267328e61e435dafc665457ed2faefebb6b465
abhiss <abbbbbbbbbbbhi@gmail.com>
2023-05-21 00:07:12 -0700

    read textmeshpro input

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,17 +22,25 @@ public class Game : MonoBehaviour
 {
     public Button RunButton;
     public GameObject player;
+    public GameObject AsmEditor;
+
     private GameState _state;
     private float _runTime;
     private int ActionDur = 1;
     private PlayerController _playerController;
+    private TextMeshProUGUI asmEditorText;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        asmEditorText = AsmEditor.GetComponent<TextMeshProUGUI>();
+        Debug.Log(asmEditorText);
         _playerController = player.GetComponent<PlayerController>();
         RunButton.GetComponent<Button>().onClick.AddListener(() =>
         {
+            var asm_text = asmEditorText.text;
+            
             _playerController.Forward();
         });
 
