80e19604e902b63f585089f79d3a832fabc5cf54
Dan <onomatopoeicboom@gmail.com>
2023-05-20 21:04:59 -0700

    add emulator and c# bindings

---
+++ interpreter/RVEmuSharp/RVEmuSharp/RVEmulator.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RVEmuSharp;
+
+public partial class RVEmulator
+{
+    private const string LIBRARY_NAME = "rvemu";
+
+
+    [DllImport(LIBRARY_NAME, EntryPoint = "emulator_create")]
+    private static extern IntPtr _CreateEmulator();
+    [DllImport(LIBRARY_NAME, EntryPoint = "emulator_load_program")]
+    private static unsafe extern IntPtr _LoadProgram(IntPtr emu, byte* array, UInt64 size);
+
+    [DllImport(LIBRARY_NAME, EntryPoint = "emulator_destroy")]
+    private static extern void _DestroyEmulator(IntPtr emu);
+
+    [DllImport(LIBRARY_NAME, EntryPoint = "emulator_cpu_execute")]
+    private static extern UInt64 _RunOnce(IntPtr emu, ref UInt64 executedInstruction);
+
+    [DllImport(LIBRARY_NAME, EntryPoint = "emulator_set_register")]
+    private static extern void _SetRegister(IntPtr emu, UInt64 index, UInt64 value);
+
+    [DllImport(LIBRARY_NAME, EntryPoint = "emulator_get_register")]
+    private static extern UInt64 _GetRegister(IntPtr emu, UInt64 index);
+
+    private readonly IntPtr instance;
+
+    public RVEmulator()
+    {
+        instance = _CreateEmulator();
+    }
+
+    ~RVEmulator()
+    {
+        _DestroyEmulator(instance);
+    }
+
+    public void LoadProgram(byte[] programBytes)
+    {
+        unsafe
+        {
+            fixed (byte* bytes = programBytes)
+            {
+                _LoadProgram(instance, bytes, (UInt64)programBytes.Length);
+            }
+        }
+
+    }
+
+    public ulong RunOnce()
+    {
+        ulong executed = 0;
+        _RunOnce(instance, ref executed);
+        return executed;
+    }
+
+    public void SetRegister(UInt64 index, UInt64 value)
+    {
+        _SetRegister(instance, index, value);
+    }
+
+    public UInt64 GetRegister(UInt64 index)
+    {
+        return _GetRegister(instance, index);
+    }
+}
---
+++ interpreter/RVEmuSharp/RVEmuSharpTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RVEmuSharp;
+
+var shellcode = new List<byte>();
+
+//add x1,x2,x3 
+//sub x1,x2,x3
+//addi x1,x2,3
+
+shellcode.AddRange(BitConverter.GetBytes((UInt32)0x003100b3));
+shellcode.AddRange(BitConverter.GetBytes((UInt32)0x403100b3));
+shellcode.AddRange(BitConverter.GetBytes((UInt32)0x00310093));
+
+
+var emulator = new RVEmulator();
+
+emulator.LoadProgram(shellcode.ToArray());
+
+for (int i = 0; i < shellcode.Count / sizeof(UInt32); i++)
+{
+    var executed = emulator.RunOnce();
+    Console.WriteLine($"Executed: {executed}");
+}
+
+Console.WriteLine($"Register 1: {emulator.GetRegister(0)}");
