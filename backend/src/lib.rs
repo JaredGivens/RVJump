7c4a48583b95c01556b189e2856b2ae9c51a5f85
abhiss <abbbbbbbbbbbhi@gmail.com>
2023-05-20 18:08:27 -0700

    asmbler done

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -6,31 +6,45 @@
 // Here we are deserializing to `serde_json::Value` but you can
 // deserialize to any other type that implementes the `Deserialize` trait.
 
+use deno_core::FastString;
 use deno_core::v8;
+use serde_v8;
 use deno_core::JsRuntime;
 use deno_core::RuntimeOptions;
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::ffi::c_char;
 
-fn add() -> {
+#[no_mangle]
+pub extern "C" fn loadAsm(instruction: *const c_char) {
+  let instruction = unsafe { CString::from(CStr::from_ptr(instruction)) };
+
   let mut runtime = JsRuntime::new(RuntimeOptions::default());
+  // let main_module = deno_core::resolve_path("Instruction.js", Path::new("C:/Users/abhi-/source/UnityProjects/RVJump/backend/encoder"));
 
-  // Evaluate some code
-  let code = "let a = 1+4; a*2";
-  let output: serde_json::Value =
-    eval(&mut runtime, code).expect("Eval failed");
+  let instruction_setup = String::from(include_str!("../encoder/Instruction.js"));
+  eval(&mut runtime, instruction_setup.into()).expect("Eval failed");
 
-    match output {
-        Value::Number(x) => x,
-        _ => panic!()
-    }
+  let binding = instruction.into_string().unwrap();
+  let instructions: Vec<&str> = binding.split("\n").collect();
 
-
+  let mut instr_memory = Vec::new();
+  
+  for instr in instructions {
+    let wrapped_instr = format!("\ninstr = new Instruction('{}'); instr.bin", instr);
+    let res = eval(&mut runtime, wrapped_instr.into()).expect("Eval failed");
+    let result_string = res.as_str().unwrap();
+    let instr_word = u32::from_str_radix(result_string, 2).unwrap();
+    instr_memory.extend(instr_word.to_le_bytes());
+    dbg!(instr_word);
+  }
 }
 
 fn eval(
   context: &mut JsRuntime,
-  code: &'static str,
+  code: FastString,
 ) -> Result<serde_json::Value, String> {
-  let res = context.execute_script_static("<anon>", code);
+  let res = context.execute_script("<anon>", code);
   match res {
     Ok(global) => {
       let scope = &mut context.handle_scope();
@@ -49,20 +63,13 @@ fn eval(
   }
 }
 
-// pub fn loadAsm(a: i32, b: i32) -> i32 {
-//     a + b
-// }
-
-
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
+    use std::ffi::CString;
+    use crate::loadAsm;
 
-    #[test]
-    fn test_add() {
-        let output = add();
-        let expected_output = (10);
-        assert_eq!(expected_output, output);
-    }
+  #[test]
+  fn test_add() {
+    loadAsm(CString::new("add x1,x2,x3\nsub x1,x2,x3\naddi x1,x2,3").unwrap().as_ptr());
+  }
 }
