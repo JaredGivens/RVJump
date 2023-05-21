823fd8427710fc7b8956fe420e8c4618d2951706
dvbuka <dvbuka@ucdavis.edu>
2023-05-20 23:35:31 -0700

    Final syntax highlighting.

--- Assets/CodeInputScript.cs
+++ Assets/CodeInputScript.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor;
+using System;
+using System.Text.RegularExpressions;
 
 public class CodeInputScript : MonoBehaviour
 {
@@ -26,24 +29,50 @@ public class CodeInputScript : MonoBehaviour
                 HighlightText();
             alreadyHighlighted = true;
             alreadyReinstated = false;
-            mainInputField.GetComponent<Image>().color = Color.green;
         }
         else {
            if (!alreadyReinstated)
                 mainInputField.GetComponent<TMP_InputField>().text = uncolored;
             uncolored = mainInputField.GetComponent<TMP_InputField>().text;
-            mainInputField.GetComponent<Image>().color = Color.red;
             alreadyHighlighted = false;
             alreadyReinstated = true;
 
         }
-        Debug.Log(mainInputField.GetComponent<TMP_InputField>().text);
     }
 
+    /* 
+     *   anything with a letter -> register,
+     *   first word of each line -> instruction
+     *   numbers -> their own color
+    */
     void HighlightText() {
         string highlighted = uncolored;
-        highlighted = highlighted.Replace("add", "<color #880808> add </color>");
-        mainInputField.GetComponent<TMP_InputField>().text = highlighted;
+       
+        string[] lines = highlighted.Split(System.Environment.NewLine);
+
+        for (int i = 0; i < lines.Length; i++) {
+            string[] line = lines[i].Split(" ");
+            
+            line[0] = "<color #880808>" + line[0] + "</color>";
+
+            for (int word_index = 1; word_index < line.Length; word_index++) {
+                string word = line[word_index];
+                if (Regex.Matches(line[word_index], @"[a-zA-Z]").Count != 0) {
+                    Debug.Log(word[word.Length - 1].CompareTo(','));
+                    if (word[word.Length - 1].CompareTo(',') == 0)
+                    {
+                        line[word_index] = "<color #00FF00>" + word.Substring(0, word.Length - 1) + "</color>" + ",";
+                    }
+                    else {
+                        line[word_index] = "<color #00FF00>" + line[word_index] + "</color>";
+
+                    }
+                }
+            }
+            lines[i] = System.String.Join(' ', line);
+        }
+
+        mainInputField.GetComponent<TMP_InputField>().text = System.String.Join(System.Environment.NewLine, lines);
     }
 }
 
